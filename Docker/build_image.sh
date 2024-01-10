2c48ea905cc6d14c711264bfae46d3284406e0e8
Enric Cervera <ecervera@uji.es>
2024-01-10 19:08:22 +0100

    Create Dockerfile and scripts

---
+++ Docker/build_image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t ghcr.io/ros-legacy/turtlebot-simulator:kinetic .
---
+++ Docker/run_container.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rocker --nvidia --x11 --name turtlebot-simulator ghcr.io/ros-legacy/turtlebot-simulator:kinetic
